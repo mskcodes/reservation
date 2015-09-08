293443cedf8ab0da371e300da3513b481d6df7cf
saihe <saihe.kyon@gmail.com>
2015-09-08 21:52:41 +0900

    accessTest.php更新

--- accessTest.php
+++ accessTest.php
@@ -1,14 +1,15 @@
 <?php
 $USER = "saihe";
-$PW   = "xwkx4pz56p";
-//パスワードが違うとエラーが出る
-$dnsinfo = "mysql:dbname=saihe_reservation;host=saihe.sakura.ne.jp;charset=utf8";
+$PW   = "kyon3141";
+$DB   = "saihe_reservation";
+$HOST = "mysql515.db.sakura.ne.jp";
+$dnsinfo = "mysql:dbname={$DB};host={$HOST};charset=utf8";
 	try{
 		$pdo = new PDO($dnsinfo,$USER,$PW);
 		$res = "接続しました";
 	}catch(PDOException $e){
-		//$res = $e->getMessage();
-		$res = "<br>接続できませんでした。";
+		$res = $e->getMessage();
+		//$res = "<br>接続できませんでした。";
 	}
 ?>
 <!DOCTYPE HTML>
@@ -20,6 +21,18 @@ $dnsinfo = "mysql:dbname=saihe_reservation;host=saihe.sakura.ne.jp;charset=utf8"
 
 <body>
 <h1>PHPでMySQLに接続する</h1>
-<?php echo $res; ?>
+<?php
+echo $res;
+echo "<br>";
+echo "<br>";
+echo "USE　:　" . $USER;
+echo "<br>";
+echo "PW　 :　" . $PW;
+echo "<br>";
+echo "HOST :　" . $HOST;
+echo "<br>";
+echo "DB　 :　" . $DB;
+echo "<br>";
+?>
 </body>
 </html>
